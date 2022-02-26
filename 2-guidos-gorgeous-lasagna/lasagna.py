632906ad0c61cea600dbbc178710c8309df234c4
Jordi <jordi.fib@gmail.com>
2022-02-26 15:07:45 +0100

    Complete exercise 2

---
+++ 2-guidos-gorgeous-lasagna/lasagna.py
@@ -0,0 +1,39 @@
+EXPECTED_BAKE_TIME = 40
+PREPARATION_TIME   = 2
+
+def bake_time_remaining (elapsed_bake_time):
+    """Calculate the bake time remaining.
+
+    :param elapsed_bake_time: int baking time already elapsed.
+    :return: int remaining bake time derived from 'EXPECTED_BAKE_TIME'.
+
+    Function that takes the actual minutes the lasagna has been in the oven as
+    an argument and returns how many minutes the lasagna still needs to bake
+    based on the `EXPECTED_BAKE_TIME`.
+    """
+
+    return (EXPECTED_BAKE_TIME - elapsed_bake_time)
+
+def preparation_time_in_minutes (number_of_layers):
+    """Calculate how many minutes you would spend making the lasagna.
+
+    :param number_of_layers: int how many lasagna layers you want to cook.
+    :return: int time required derived from 'EXPECTED_BAKE_TIME'.
+
+    Function that takes how many lasagna layers you want to cook and returns
+    how many minutes you will spend on this task, based on the `PREPARATION_TIME`.
+    """
+
+    return (PREPARATION_TIME * number_of_layers)
+
+def elapsed_time_in_minutes (number_of_layers, elapsed_bake_time):
+    """
+    Return elapsed cooking time.
+
+    This function takes two numbers representing the number of layers & the time already spent
+    baking and calculates the total elapsed minutes spent cooking the lasagna.
+    """
+
+    return (
+        preparation_time_in_minutes (number_of_layers) + elapsed_bake_time
+    )
--- Makefile
+++ Makefile
@@ -2,3 +2,7 @@
 exercise-1-test:
 	python3 -m pytest 1-hello-world/hello_world_test.py
 .PHONY: exercise-1-test
+
+exercise-2-test:
+	python3 -m pytest 2-guidos-gorgeous-lasagna/lasagna_test.py
+.PHONY: exercise-2-test
